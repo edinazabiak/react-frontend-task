825e657251ed1f25ffab9bf9535a031d96a2e7a1
Zabiak Edina <edi.zabi@gmail.com>
2022-06-04 00:10:11 +0200

    Kész a Highlight rész

--- src/components/highlight/Highlight.jsx
+++ src/components/highlight/Highlight.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './highlight.css'
+import { BsArrowRight } from 'react-icons/bs'
 
 export default function Highlight() {
 
     return (
-        <></>
+        <section className='container'>
+            <div className="highlight__container">
+                <h2>A természetes szépségért</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+                <button className='btn btn__primary'>
+                    Megnézem
+                    <BsArrowRight className='btn__icon' />
+                </button>
+            </div>
+        </section>
     )
 }
